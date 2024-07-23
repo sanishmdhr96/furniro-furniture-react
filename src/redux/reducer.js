18f5ab826003d90a0c359482632302116950ab2f
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2024-07-23 19:37:56 +0545

    redux usages

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,12 @@
 
 import { combineReducers } from '@reduxjs/toolkit'
 import { productReducer } from '../duck/products/reducer'
+import { userReducer } from '../duck/users/reducer'
 
 
 const appReducer = combineReducers({
-    product: productReducer
+    product: productReducer,
+    user: userReducer
 })
 
 const rootReducer = (state, action) => {
